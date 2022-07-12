0b2b196d2657db4c4d2a29a265c199b896cc855e
Karina Sang <ycsang@uwaterloo.ca>
2022-07-12 16:59:40 -0400

    added week3 slides

---
+++ AP Java/Week3/BankAccount.java
@@ -0,0 +1,60 @@
+package week3;
+
+public class BankAccount {
+	private String password;
+	private double balance;
+	public static final double OVERDRAWN_PENALTY = 20.00;
+
+	//constructors
+	/** Default constructor.
+	 * Constructs bank account with default values. */
+	public BankAccount()
+	{ 
+		password = "hello";
+	}
+
+
+	/** Constructs bank account with specified password and balance. */
+	public BankAccount(String acctPassword, double acctBalance)
+	{ 
+		password = acctPassword;
+		balance = acctBalance;
+	}
+
+
+	// accessor
+	/** @return balance of this account */
+	public double getBalance()
+	{ 
+		return balance;
+	}
+
+
+	// mutators
+	/** Deposits amount in bank account with given password.
+	 * @param acctPassword the password of this bank account
+	 * @param amount the amount to be deposited
+	 */
+	public void deposit(String acctPassword, double amount)
+	{ 
+		if (password.equals(acctPassword)) {
+			balance += amount;
+		}
+			
+	}
+
+
+	/** Withdraws amount from bank account with given password.
+	 * Assesses penalty if balance is less than amount.
+	 * @param acctPassword the password of this bank account
+	 * @param amount the amount to be withdrawn
+	 */
+	public void withdraw(String acctPassword, double amount)
+	{ 
+		if (password.equals(acctPassword)) {
+			balance -= amount;
+		}
+	}
+
+}
+
---
+++ AP Java/Week3/Test.java
@@ -0,0 +1,11 @@
+package week3;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Person person1 = new Person("Name", 20);
+		person1.printPerson();
+
+	}
+
+}
